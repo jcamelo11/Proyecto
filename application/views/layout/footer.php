29c2bcd81c52acf5012a70f89f5f3980e6a3cc99
unknown <SENA@VALAPRCBCFSP200.FormacionSena.RED>
2022-11-30 08:26:19 -0500

    first commit

---
+++ application/views/layout/footer.php
@@ -0,0 +1,45 @@
+      <!-- Footer -->
+      <footer class="footer pt-0">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-6">
+            <div class="copyright text-center  text-lg-left  text-muted">
+              SISTEMA DE VENTAS DESARROLLADO POR<br/>
+              YULIANA MIRANDA <a href="https://sapd10.000webhostapp.com" class="font-weight-bold ml-1" target="_blank">Lights Thing</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="copyright text-center  text-lg-left  text-muted">
+              CONTACTANOS  <br/>
+              Email: fcespedes3204027459@gmail.com<br/>
+              Tel: 320 402 74 59<br/>
+              Dir: Crr 18 e # 27 -13 1ro Mayo<br/>
+              &copy; 2023 Papelería EL Rey
+            </div>
+          </div>
+
+        </div>
+      </footer>
+    </div>
+  </div>
+  <!-- Argon Scripts -->
+  <!-- Core -->
+  <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/jquery-scroll-lock/jquery-scrollLock.min.js"></script>
+  <!-- Optional JS -->
+  <script src="<?php echo base_url(); ?>assets/vendor/chart.js/Chart.min.js"></script>
+  <!-- Datatable -->
+  <script src="<?php echo base_url(); ?>assets/vendor/datatables.net/jquery.dataTables.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/datatables.net-responsive-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <!-- Sweet Alert 2 -->
+  <script src="<?php echo base_url(); ?>assets/vendor/sweetalert2/sweetalert2.min.js"></script>
+
+  <script src="<?php echo base_url(); ?>assets/vendor/moment.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/bootstrap-datetimepicker.js"></script>
+  
+  <!-- Argon JS -->
+  <script src="<?php echo base_url(); ?>assets/js/argon.js"></script>
+</body>
+
+</html>
